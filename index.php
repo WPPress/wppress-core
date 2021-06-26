96a06aba2f8112a837285afefb1d602a68483cc5
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2021-06-26 15:19:18 +0545

    initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+Plugin Name: WPPress Core
+Plugin URI: https://codecanyon.net/user/wppress
+Description: Adds core functions/libraries used by products made by WPPress team.
+Version: 1.0.0
+Author: WPPress
+Author URI: https://codecanyon.net/user/wppress
+License: GNU General Public License v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wppress-tiktok-box
+ */
+
+ require("./vendor/autoload.php");
